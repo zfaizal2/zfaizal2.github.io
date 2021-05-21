e9cc2f8736864aafd302c16a11a762ab82b862f0
Zayyan Faizal <zfaizal2@illinois.edu>
2021-05-20 23:24:32 -0500

    new style

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,7 @@ import {
 export default () =>
     
     <Layout style={{overflow:"scroll"}}>
+
         <script data-goatcounter="https://zayyan.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
         <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'/>
         <Helmet>
@@ -26,22 +27,22 @@ export default () =>
           <link rel="canonical" href="https://zayyanfaizal.com/" />
         </Helmet>
         <div class="container-fluid">
-        <div class="ui stackable grid" style={{height:"-webkit-fill-available", minHeight: "100%", fontFamily:"Poppins", lineHeight:"24px", }}>
-            <div class="stackable centered column" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height:"100vh", marginTop:"auto", marginBottom:"auto"}}>
-                <Card  style={{flexDirection:"height", width:"75%", textAlign:"center", boxShadow:"0 0px 20px 0 #d4d4d5, 0 0 0 0px #d4d4d5",borderRadius:"0rem",overflow:"visible"}}>
-                        <div class="ui stackable grid" style={{height:"100%",margin:"0",display:"inline"}}>
-                            <div class="row" style={{maxWidth:"100",maxHeight:"25%",alignItems:"center",padding:"8px",justifyContent:'center',marginLeft:"auto", marginRight:"auto"}}>
+        <div class="ui stackable grid" style={{height:"-webkit-fill-available", minHeight: "100%", fontFamily:"comicsans", lineHeight:"24px", }}>
+            <div class="stackable centered column" style={{display: 'flex',  justifyContent:'left', alignItems:'left', height:"100vh", marginTop:"auto", marginBottom:"auto"}}>
+                {/* <Card  style={{flexDirection:"height", width:"85%", textAlign:"center", boxShadow:"0 0px 20px 0 #d4d4d5, 0 0 0 0px #d4d4d5",borderRadius:"0rem",overflow:"visible"}}> */}
+                        <div class="ui stackable grid" style={{height:"100%",margin:"0",display:"inline", width:'100%'}}>
+                            <div class="row" style={{maxWidth:"100",maxHeight:"25%",alignItems:"left",padding:"15px",justifyContent:'left',marginLeft:"auto", marginRight:"auto"}}>
                                 <div class="column" style={{padding:'8px',float:"left",width:"125px"}}>
                                     <img src={Img} style={{maxHeight:"100px",borderRadius:"50%"}}></img> 
                                 </div>
-                                <div class="column" style={{padding:"4px", flexDirection:"height", height:"100px", maxWidth:"100%", width:"30%", marginTop:"2%", textAlign:"center"}}>
-                                    <p style={{ textAlign:"center", fontSize:"3rem"}}><strong>hi, i'm Zayyan.</strong></p>
+                                <div class="column" style={{padding:"4px", flexDirection:"height", height:"100px", maxWidth:"100%", width:"30%", marginTop:"2%", textAlign:"left"}}>
+                                    <p style={{ textAlign:"left", fontSize:"3rem"}}><>zayyan faizal.</></p>
                                 </div>
                             </div>
-                            <GridRow class = "two column row" style={{clear:"both",padding:'8px'}}>
-                                <GridColumn style={{float:"left",width:"50%", marginTop:"2"}}>
-                                    <div style={{padding:"8px", fontSize:"1.5rem",textAlign:'left', marginTop:"1%",}}> 
-                                        <strong>education</strong>
+                            <GridRow class = "two column row" style={{clear:"both",paddingLeft:'8px', alignItems:'center', width:'100%'}}>
+                                <GridColumn style={{float:"right",width:"50%", marginTop:"2", alignItems:"right"}}>
+                                    <div style={{padding:"8px", fontSize:"2rem",textAlign:'left', marginTop:"1%",fontStyle:'italic', textDecoration:'underline'}}> 
+                                        <>education</>
                                     </div>
                                     <GridRow style={{padding:"8px", fontSize:"1.5rem",textAlign:'left'}}>
                                             <div >systems engineering & design</div>
@@ -56,13 +57,10 @@ export default () =>
                                             <div >class of 2022</div>
                                     </GridRow>
                                 </GridColumn>
-                                <GridColumn style={{float:"left",width:"50%"}}>
-                                    <div style={{padding:"8px", fontSize:"1.5rem",textAlign:'left',marginTop:"1%"}}> 
-                                        <strong>currently</strong> 
+                                <GridColumn style={{float:"center",width:"50%"}}>
+                                    <div style={{padding:"8px", fontSize:"2rem",textAlign:'left', marginTop:"0%",fontStyle:'italic', textDecoration:'underline'}}>
+                                        <>currently</> 
                                     </div>
-                                    <GridRow style={{padding:"8px", fontSize:"1.5rem",textAlign:'left'}}>
-                                            <a >swe intern @ motorola solutions 2020-2021</a>
-                                    </GridRow>
                                     <GridRow style={{padding:"8px", fontSize:"1.5rem",textAlign:'left'}}>
                                             <a href="https://uiuc.hack4impact.org/">product research lead @ hack4impact</a>
                                     </GridRow>
@@ -72,17 +70,23 @@ export default () =>
                                      <GridRow style={{padding:"8px", fontSize:"1.5rem",textAlign:'left'}}>
                                             <a href="https://www.notion.so/ryoske/my-work-146c19ab2aa8475980f8db68ea750f64">side projects ---></a>
                                     </GridRow> 
+                                    <div style={{padding:"8px", fontSize:"2rem",textAlign:'left', marginTop:"1%",fontStyle:'italic', textDecoration:'underline'}}> 
+                                        <>formerly</> 
+                                    </div>
+                                    <GridRow style={{padding:"8px", fontSize:"1.5rem",textAlign:'left'}}>
+                                            <a >swe intern @ motorola solutions 2020-2021</a>
+                                    </GridRow>
                                 </GridColumn>
                             </GridRow>
                             <GridRow style={{margin:"2%", marginRight:"2%", padding:"0rem", justifyContent:"center", alignItems:"center", clear:"both", height:"10%", display:"block"}}>
                                     <a style={{color:"black", margin:"12px", fontSize:"1.75em"}} target="_blank" href="https://twitter.com/brownmanwonders"><FontAwesomeIcon icon={faTwitter}/></a>
                                     <a style={{color:"black", margin:"12px", fontSize:"1.75em"}} target="_blank" href="https://linkedin.com/in/zayyanfaizal/"><FontAwesomeIcon icon={faLinkedinIn}/></a>
-                                    <a style={{color:"black", margin:"12px", fontSize:"1.75em"}}target="_blank" href="https://github.com/zfaizal2"><FontAwesomeIcon icon={faGithub}/></a> 
+                                    <a style={{color:"black", margin:"12px", fontSize:"1.75em"}} target="_blank" href="https://github.com/zfaizal2"><FontAwesomeIcon icon={faGithub}/></a> 
                                     <a style={{color:"black", margin:"12px", fontSize:"1.75em"}} target="_blank" href="https://medium.com/@zayyanfaizal"><FontAwesomeIcon icon={faMediumM}/></a>
                                     <a style={{color:"black", margin:"12px", fontSize:"1.75em"}} target="_blank" href="mailto:zfaizal2@illinois.edu"><FontAwesomeIcon icon={faEnvelope}/></a> 
                             </GridRow>
                         </div>
-                </Card>
+                {/* </Card> */}
             </div>
         </div>
         </div>
