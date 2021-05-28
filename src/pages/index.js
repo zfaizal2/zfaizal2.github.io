97f5c937f5a640e79e67ff8cfe931cff7eaf6392
Zayyan Faizal <zfaizal2@illinois.edu>
2021-05-27 23:05:40 -0500

    standardized styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "./layout"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter,faLinkedinIn,faGithub,faMediumM,faen } from "@fortawesome/free-brands-svg-icons";
 import { Helmet } from "react-helmet"
-import {socialIcon, list} from "./index.modules.css"
+import "./index.modules.css"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import {
     Card, 
@@ -30,21 +30,21 @@ export default () =>
                 <div class="stackable centered column" style={{display: 'flex', justifyContent:'center', alignItems:'center', height:"100vh", marginTop:"auto", marginBottom:"auto"}}>
                     <div class="ui stackable grid" style={{height:"100%",display:"inline"}}>
                         <div class="row" style={{alignItems:"center",padding:"8px",justifyContent:'center',marginLeft:"auto", marginRight:"auto"}}>
-                                <img src={Img} style={{maxWidth:"30%",borderRadius:"50%"}}></img> 
-                                <p style={{textAlign:"center", fontSize:"8vw",fontStyle:'italic', textDecoration:'underline', padding:"3vw"}}>Zayyan Faizal</p>
+                                <img className="headshot" src={Img} ></img> 
+                                <p className="name">Zayyan Faizal</p>
                         </div>
                         <GridRow style={{clear:"both",padding:'8px', alignItems:'center', textAlign:'center'}}>
-                            <div className={"list"}>education</div>
-                            <div style={{width:'100%', textAlign:"center", fontSize:"8vw",fontStyle:'italic', textDecoration:'underline', padding:"3vw"}}>work</div>
-                            <div style={{width:'100%', textAlign:"center", fontSize:"8vw",fontStyle:'italic', textDecoration:'underline', padding:"3vw"}}>projects</div>
-                            <div style={{width:'100%', textAlign:"center", fontSize:"8vw",fontStyle:'italic', textDecoration:'underline', padding:"3vw"}}>interests</div>
+                            <div className="list">education</div>
+                            <div className="list">work</div>
+                            <div className="list">projects</div>
+                            <div className="list">interests</div>
                         </GridRow>
                         <GridRow style={{justifyContent:"center", alignItems:"center", height:"20%", display:"block"}}>
-                                <div className={socialIcon} href="https://twitter.com/brownmanwonders"><FontAwesomeIcon icon={faTwitter}/></div>
-                                <a class="social-icon" target="_blank" href="https://linkedin.com/in/zayyanfaizal/"><FontAwesomeIcon icon={faLinkedinIn}/></a>
-                                <a class="social-icon" target="_blank" href="https://github.com/zfaizal2"><FontAwesomeIcon icon={faGithub}/></a> 
-                                <a class="social-icon" target="_blank" href="https://medium.com/@zayyanfaizal"><FontAwesomeIcon icon={faMediumM}/></a>
-                                <a class="social-icon" target="_blank" href="mailto:zfaizal2@illinois.edu"><FontAwesomeIcon icon={faEnvelope}/></a> 
+                                <a className="social-icon" rel="noreferrer" target="_blank" href="https://twitter.com/brownmanwonders"><FontAwesomeIcon icon={faTwitter}/></a>
+                                <a className="social-icon" rel="noreferrer" target="_blank" href="https://linkedin.com/in/zayyanfaizal/"><FontAwesomeIcon icon={faLinkedinIn}/></a>
+                                <a className="social-icon" rel="noreferrer" target="_blank" href="https://github.com/zfaizal2"><FontAwesomeIcon icon={faGithub}/></a> 
+                                <a className="social-icon" rel="noreferrer" target="_blank" href="https://medium.com/@zayyanfaizal"><FontAwesomeIcon icon={faMediumM}/></a>
+                                <a className="social-icon" rel="noreferrer" target="_blank" href="mailto:zfaizal2@illinois.edu"><FontAwesomeIcon icon={faEnvelope}/></a> 
                         </GridRow>
                     </div>
                 </div>
