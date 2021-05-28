52ed8aac83efc72c3bb7b3bbadd631e7b4f73d42
Zayyan Faizal <zfaizal2@illinois.edu>
2021-05-28 01:39:27 -0500

    hover for details on work and education

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useHover} from "react"
 import "semantic-ui-css/semantic.min.css";
 import Img from "./zayyan_faizal.jpg"
 import Layout from "./layout"
@@ -15,7 +15,7 @@ import {
 
 
 
-export default () =>
+export default () => 
 
     <Layout style={{overflow:"scroll"}}>
         <script data-goatcounter="https://zayyan.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
@@ -29,17 +29,25 @@ export default () =>
             <div class="ui stackable grid" style={{height:"-webkit-fill-available", minHeight: "100%", fontFamily:"Times_New_Roman", lineHeight:"24px"}}>
                 <div class="stackable centered column" style={{display: 'flex', justifyContent:'center', alignItems:'center', height:"100vh", marginTop:"auto", marginBottom:"auto"}}>
                     <div class="ui stackable grid" style={{height:"100%",display:"inline"}}>
-                        <div class="row" style={{alignItems:"center",padding:"8px",justifyContent:'center',marginLeft:"auto", marginRight:"auto"}}>
+                        <GridRow style={{alignItems:"center",padding:"8px",justifyContent:'center',marginLeft:"auto", marginRight:"auto"}}>
                                 <img className="headshot" src={Img} ></img> 
                                 <p className="name">Zayyan Faizal</p>
-                        </div>
+                        </GridRow>
                         <GridRow style={{clear:"both",padding:'8px', alignItems:'center', textAlign:'center'}}>
                             <div className="list">education</div>
+                            <div className="hide">university of illinois at urbana champaign <br/> class of 2022</div>
                             <div className="list">work</div>
+                            <div className="hide"> 
+                                <div class="subheader">currently</div>
+                                pm intern @ microsoft <br/> 
+                                software dev @ hack4impact <br/>
+                                <div class="subheader">previously</div>
+                                swe intern @ motorola solutions
+                            </div>
                             <div className="list">projects</div>
                             <div className="list">interests</div>
                         </GridRow>
-                        <GridRow style={{justifyContent:"center", alignItems:"center", height:"20%", display:"block"}}>
+                        <GridRow style={{textAlign:"center", display:"block"}}>
                                 <a className="social-icon" rel="noreferrer" target="_blank" href="https://twitter.com/brownmanwonders"><FontAwesomeIcon icon={faTwitter}/></a>
                                 <a className="social-icon" rel="noreferrer" target="_blank" href="https://linkedin.com/in/zayyanfaizal/"><FontAwesomeIcon icon={faLinkedinIn}/></a>
                                 <a className="social-icon" rel="noreferrer" target="_blank" href="https://github.com/zfaizal2"><FontAwesomeIcon icon={faGithub}/></a> 
